9843e947ab698205d6eb388dd0beda615f341482
Ishwor Khadka <ishwork28@gmail.com>
2022-12-29 10:01:43 +0545

    changing the theme color

--- lib/app/theme.dart
+++ lib/app/theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 ThemeData getApplicationThemeData() {
   return ThemeData(
-    primaryColor: HexColor.fromHex("#ED9728"),
+    primaryColor: HexColor.fromHex("#1e81b0"),
     primaryColorLight: HexColor.fromHex("#B3ED9728"),
     primaryColorDark: HexColor.fromHex("#d17d11"),
     disabledColor: HexColor.fromHex("#707070"),
@@ -10,12 +10,12 @@ ThemeData getApplicationThemeData() {
     buttonTheme: ButtonThemeData(
       shape: const StadiumBorder(),
       disabledColor: HexColor.fromHex("#707070"),
-      buttonColor: HexColor.fromHex("#ED9728"),
+      buttonColor: HexColor.fromHex("#1e81b0"),
       splashColor: HexColor.fromHex("#B3ED9728"),
     ),
     appBarTheme: AppBarTheme(
       centerTitle: true,
-      color: HexColor.fromHex("#ED9728"),
+      color: HexColor.fromHex("#1e81b0"),
       elevation: 4,
       shadowColor: HexColor.fromHex("#B3ED9728"),
       titleTextStyle: const TextStyle(
@@ -31,7 +31,7 @@ ThemeData getApplicationThemeData() {
           color: Colors.white,
           fontWeight: FontWeight.w500,
         ),
-        backgroundColor: HexColor.fromHex("#ED9728"),
+        backgroundColor: HexColor.fromHex("#1e81b0"),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12),
         ),
@@ -58,7 +58,7 @@ ThemeData getApplicationThemeData() {
       //Focus border
       focusedBorder: OutlineInputBorder(
           borderSide:
-              BorderSide(color: HexColor.fromHex("#ED9728"), width: 1.5),
+              BorderSide(color: HexColor.fromHex("#1e81b0"), width: 1.5),
           borderRadius: const BorderRadius.all(Radius.circular(8))),
       // error border
       errorBorder: OutlineInputBorder(
@@ -67,7 +67,7 @@ ThemeData getApplicationThemeData() {
           borderRadius: const BorderRadius.all(Radius.circular(8))),
       focusedErrorBorder: OutlineInputBorder(
           borderSide:
-              BorderSide(color: HexColor.fromHex("#ED9728"), width: 1.5),
+              BorderSide(color: HexColor.fromHex("#1e81b0"), width: 1.5),
           borderRadius: const BorderRadius.all(Radius.circular(8))),
       // enabled border
       enabledBorder: OutlineInputBorder(
